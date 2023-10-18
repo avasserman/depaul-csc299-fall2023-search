5dabe2996d4a5148921ef9f53b28b0ee76af31be
Alexander Vasserman <alvasserman@gmail.com>
2023-10-18 12:44:17 -0400

    query process class and run_search function.

--- query_process.py
+++ query_process.py
@@ -1,5 +1,6 @@
-from documents import DocumentStore
+from documents import DocumentStore, DictDocumentStore
 from index import Index, BaseIndex
+from tf_idf_index import TfIdfIndex
 from tokenizer import tokenize
 
 
@@ -15,7 +16,29 @@ def format_out(results: list[str], document_store: DocumentStore, unused_process
     return output_string
 
 
-def query_process(document_store: DocumentStore, index: BaseIndex, query: str, number_of_results: int) -> str:
-    processed_query = preprocess_query(query)
-    results = index.search(processed_query, number_of_results)
-    return format_out(results, document_store, processed_query)
+class QueryProcess:
+    def __init__(self, document_store: DocumentStore, index: BaseIndex):
+        self.document_store = document_store
+        self.index = index
+
+    @staticmethod
+    def create(document_store_path: str, index_path: str) -> 'QueryProcess':
+        return QueryProcess(document_store=DictDocumentStore.read(document_store_path),
+                            index=TfIdfIndex.read(index_path))
+
+    def search(self, query: str, number_of_results: int) -> str:
+        processed_query = preprocess_query(query)
+        results = self.index.search(processed_query, number_of_results)
+        return format_out(results, self.document_store, processed_query)
+
+
+def run_search():
+    qp = QueryProcess.create('', '')
+    query = input('Please enter your query:')
+    while query:
+        print(qp.search(query=query, number_of_results=10))
+        query = input('Please enter your query:')
+
+
+if __name__ == '__main__':
+    run_search()
